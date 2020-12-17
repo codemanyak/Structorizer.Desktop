be2e857c1807006e5b006203d4db2e7a21e57c3c
codemanyak <kay.guertzig@fh-erfurt.de>
2020-12-17 05:50:31 +0100

    Some superfluous imports removed in TurtleBox.java

--- src/lu/fisch/turtle/TurtleBox.java
+++ src/lu/fisch/turtle/TurtleBox.java
@@ -71,7 +71,6 @@ import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -82,7 +81,6 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -101,7 +99,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 
 import lu.fisch.diagrcontrol.*;
@@ -1789,7 +1786,7 @@ public class TurtleBox implements DelayableDiagramController
 
         alpha = alpha/Math.PI*180;
 
-        // rounding (FIXME: what for?)
+        // rounding (FIXME: what for?) Isn't this actually the same as Math.round(alpha)?
         alpha = Math.round(alpha*100)/100;
 
         if (cosAlpha < 0) // Q2 & Q3
